80dd25294b0bb6a270d0abc445b44055d18b6342
Psychopath21312 <rodionr07@gmail.com>
2025-08-15 00:27:19 +0400

    Correct Tab kbd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,31 +1,34 @@
-local tab_settings = vim.api.nvim_create_augroup("TabSettings", { clear = true })
+-- Общие параметры табуляции
+vim.opt.expandtab = true -- Пробелы вместо табов
+vim.opt.smartindent = true -- Умное автоотступление
+vim.opt.autoindent = true
+vim.opt.tabstop = 4 -- Кол-во пробелов за таб по умолчанию
+vim.opt.shiftwidth = 4 -- Шаг смещения по умолчанию
+vim.opt.softtabstop = 4
 
+-- Настройки под конкретные языки
+local lang_indents = {
+	lua = { shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+	python = { shiftwidth = 4, tabstop = 4, softtabstop = 4 },
+	javascript = { shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+	typescript = { shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+	html = { shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+	css = { shiftwidth = 2, tabstop = 2, softtabstop = 2 },
+}
+
+-- Автокоманда, чтобы менять отступы при открытии файла
 vim.api.nvim_create_autocmd("FileType", {
-	group = tab_settings,
-	pattern = { "python", "lua", "css", "scss" },
-	callback = function()
-		vim.bo.tabstop = 4
-		vim.bo.shiftwidth = 4
-		vim.bo.expandtab = true
+	pattern = "*",
+	callback = function(args)
+		local ft = args.match
+		if lang_indents[ft] then
+			for opt, val in pairs(lang_indents[ft]) do
+				vim.opt_local[opt] = val
+			end
+		end
 	end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-	group = tab_settings,
-	pattern = { "c", "cpp" },
-	callback = function()
-		vim.bo.tabstop = 4
-		vim.bo.shiftwidth = 4
-		vim.bo.expandtab = false
-	end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-	group = tab_settings,
-	pattern = { "yuck" },
-	callback = function()
-		vim.bo.tabstop = 8
-		vim.bo.shiftwidth = 8
-		vim.bo.expandtab = true
-	end,
-})
+-- Маппинг для Tab / Shift-Tab как смещения в визуальном режиме
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true })
